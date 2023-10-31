1664cd1228997067bca29d2ec312b2cca530fd4d
Adrian Glos <adrian@Adrians-MBP.fritz.box>
2023-10-31 17:44:55 +0100

    Load the kaya Function and test it

---
+++ code.R
@@ -0,0 +1,15 @@
+#Load the kaya Function
+source("Test.Package/R/functions.R")
+
+#Set the Values
+pop = 82.4
+gdp=44
+enInt=5
+carbInt=0.05
+
+#Test the Function
+co2 <- kaya(pop = pop, gdp = gdp, enInt = enInt, carbInt = carbInt)
+
+#Test for negative 'pop' value
+pop_neg = -82.4
+co2_neg <- kaya(pop = pop_neg, gdp = gdp, enInt = enInt, carbInt = carbInt)
