374e0f2b5c9aef167a96fe9f59c90cefa24d6284
Adrian Glos <adrian@Adrians-MBP.fritz.box>
2023-10-31 17:45:18 +0100

    Implementation of the Kaya Function

--- Test.Package/R/functions.R
+++ Test.Package/R/functions.R
@@ -1,18 +1,23 @@
+#' Kaya
+#'
+#' @param pop Population size (in millions)
+#' @param gdp GDP per capita (in 1000$/person)
+#' @param enInt Energy intensity (in Gigajoule/$1000GDP). Energy intensity is
+#' the energy needed to produce a certain amount of economic value.
+#' @param carbInt Carbon intensity (in tonnes CO2/Gigajoule). Carbon intensity 
+#' is the CO2 emitted for produced energy. This number depends on the energy 
+#' mix used (coal, solar, . . . ).
+#'
+#' @return co2: 
+#' @export
+#'
+#' @examples co2 <- kaya(pop = 82.4, gdp=44, enInt=5, carbInt=0.05)
+
 kaya <- function(pop, gdp, enInt, carbInt){
-  #' Kaya
-  #'
-  #' @param pop Population size (in millions)
-  #' @param gdp GDP per capita (in 1000$/person)
-  #' @param enInt Energy intensity (in Gigajoule/$1000GDP). Energy intensity is
-  #' the energy needed to produce a certain amount of economic value.
-  #' @param carbInt Carbon intensity (in tonnes CO2/Gigajoule). Carbon intensity 
-  #' is the CO2 emitted for produced energy. This number depends on the energy 
-  #' mix used (coal, solar, . . . ).
-  #'
-  #' @return co2: 
-  #' @export
-  #'
-  #' @examples co2 <- kaya(pop = 82.4, gdp=44, enInt=5, carbInt=0.05)
+  if(!require("checkmate")) {
+    install.packages("checkmate")
+  }
+  checkmate::assert(pop > 0, gdp > 0, enInt > 0, carbInt > 0, combine = "and")
   co2 = pop * gdp * enInt * carbInt
   return(co2)
 }
