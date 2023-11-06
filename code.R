d5b46e77043d98465318ec8c7aa4f586a47dcd2b
Adrian Glos <adrian@Adrians-MBP.fritz.box>
2023-11-06 10:10:17 +0100

    Unit Tests via test_that instead of just the function

--- code.R
+++ code.R
@@ -1,21 +1,23 @@
 #Load the kaya Function
 source("Test.Package/R/functions.R")
+library(testthat)
 
 #Set the Values
 pop = 82.4
+pop_neg
 gdp=44
 enInt=5
 carbInt=0.05
 
-#Test the Function
-co2 <- kaya(pop = pop, gdp = gdp, enInt = enInt, carbInt = carbInt)
+#Unit Tests
+test_that("Test of the Function kaya", {
+  #Test the normal Function
+  expect_equal(kaya(pop = pop, gdp = gdp, enInt = enInt, carbInt = carbInt), 906.4)
+  #Test for negative 'pop' value
+  expect_error(kaya(pop = pop_neg, gdp = gdp, enInt = enInt, carbInt = carbInt))
+  #Test for Carbon
+  expect_equal(kaya(pop = pop, gdp = gdp, enInt = enInt, carbInt = carbInt, output_type = "C"), 246.975476839237)
+  #Test for invalid "output_type"
+  expect_error(kaya(pop = pop, gdp = gdp, enInt = enInt, carbInt = carbInt, output_type = "carbonator"))
+})
 
-#Test for negative 'pop' value
-pop_neg = -82.4
-co2_neg <- kaya(pop = pop_neg, gdp = gdp, enInt = enInt, carbInt = carbInt)
-
-#Test for Carbon
-carbon = kaya(pop = pop, gdp = gdp, enInt = enInt, carbInt = carbInt, output_type = "C")
-
-#Test for Invalid 'output_type'
-carbonator = kaya(pop = pop, gdp = gdp, enInt = enInt, carbInt = carbInt, output_type = "carbonator")
